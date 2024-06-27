41dd93f2ce97ab4369b986d3e70f6742c09476f5
Bhavesh Saluja <salujabhavesh24@gmail.com>
2024-06-27 17:17:34 +0530

    added more failing test cases

--- spec/lib/my_string_calculator_spec.rb
+++ spec/lib/my_string_calculator_spec.rb
@@ -1,4 +1,3 @@
-# spec/lib/string_calculator_spec.rb
 require 'rails_helper'
 require 'my_string_calculator'
 
@@ -7,6 +6,16 @@ RSpec.describe MyStringCalculator, type: :model do
     it 'returns 0 for an empty string' do
       calculator = MyStringCalculator.new
       expect(calculator.add("")).to eq(0)
+    end
+
+    it 'returns the number for a single number string' do
+      calculator = MyStringCalculator.new
+      expect(calculator.add("1")).to eq(1)
+    end
+
+    it 'returns the sum of two numbers' do
+      calculator = MyStringCalculator.new
+      expect(calculator.add("1,2")).to eq(3)
     end
   end
 end
